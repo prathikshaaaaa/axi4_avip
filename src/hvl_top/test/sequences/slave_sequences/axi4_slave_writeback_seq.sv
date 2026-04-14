a94d5af33e2ccec6c276f528a2b27966fc4596c7
Sharath Kumar <143931064+SharathKumar246@users.noreply.github.com>
2026-04-14 16:59:41 +0530

    added axi4_slave_writeback_seq.sv

---
+++ src/hvl_top/test/sequences/slave_sequences/axi4_slave_writeback_seq.sv
@@ -0,0 +1,29 @@
+class axi4_slave_writeback_seq extends axi4_slave_bk_base_seq;
+  `uvm_object_utils(axi4_slave_writeback_seq)
+ 
+  function new(string name="axi4_slave_writeback_seq");
+    super.new(name);
+  endfunction
+ 
+  task body();
+    super.body();
+ 
+    start_item(req);
+ 
+    if(!req.randomize() with {
+      req.tx_type       == WRITE;
+      req.transfer_type == BLOCKING_WRITE;
+ 
+      req.awburst == WRITE_INCR;
+      req.awsize  == WRITE_4_BYTES;
+ 
+      // full cache line
+      req.awlen == WORDS_PER_LINE - 1;
+    }) begin
+      `uvm_fatal("SLV_WB_SEQ", "Rand failed");
+    end
+ 
+    finish_item(req);
+  endtask
+ 
+endclass
