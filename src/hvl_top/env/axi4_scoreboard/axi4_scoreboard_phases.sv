92ff10914f6f4b0f91e88e611308f3d587d2c5b9
Sharath Kumar <143931064+SharathKumar246@users.noreply.github.com>
2026-04-15 16:10:30 +0530

    Update axi4_scoreboard_phases.sv

--- src/hvl_top/env/axi4_scoreboard/axi4_scoreboard_phases.sv
+++ src/hvl_top/env/axi4_scoreboard/axi4_scoreboard_phases.sv
@@ -1,6 +1,6 @@
 
 
-function axi4_scoreboard::new(string name = "axi4_scoreboard", 
+function new(string name = "axi4_scoreboard", 
                                        uvm_component parent = null);
   super.new(name, parent);
 endfunction : new
