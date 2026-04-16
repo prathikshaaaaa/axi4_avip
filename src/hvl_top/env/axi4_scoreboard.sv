cc5026b08ae88ce610a9735fd7f6c0512568ba13
tejas-poojary <tejaspoojary2003@gmail.com>
2026-04-16 16:18:16 +0530

    Update axi4_scoreboard.sv

--- src/hvl_top/env/axi4_scoreboard.sv
+++ src/hvl_top/env/axi4_scoreboard.sv
@@ -116,7 +116,7 @@ typedef struct {
    bit needs_writeback;
    bit wb_done;
    bit wb_error;
-   bit [1:0] resp_code;
+   rresp_e resp_code;
 
    // ---------------- Write Data Buffer 
   logic [DATA_WIDTH-1:0] wdata_buf[WORDS_PER_LINE];
