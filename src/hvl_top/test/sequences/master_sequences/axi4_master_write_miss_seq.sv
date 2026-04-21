8a522815be04d41dc08d3ef2277336438d895c56
Sharath Kumar <143931064+SharathKumar246@users.noreply.github.com>
2026-04-21 15:07:09 +0530

    Update axi4_master_write_miss_seq.sv

--- src/hvl_top/test/sequences/master_sequences/axi4_master_write_miss_seq.sv
+++ src/hvl_top/test/sequences/master_sequences/axi4_master_write_miss_seq.sv
@@ -30,7 +30,7 @@ class axi4_master_write_miss_seq extends axi4_master_base_seq;
       req.awlen   == 3;              // 4 beats
       req.awcache == READ_WRITE_ALLOCATE;
       
-      req.awaddr == 32'h8000_1000;
+      req.awaddr == 32'h0000_0001;
 
     }) begin
       `uvm_fatal("AXI4_WRITE_MISS_SEQ", "Randomization failed")
