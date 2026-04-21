71843578aceb5420626a3cc834fb13b57211a7aa
Sharath Kumar <143931064+SharathKumar246@users.noreply.github.com>
2026-04-21 14:51:19 +0530

    Update axi4_decoder.sv

--- src/hdl_top/axi4_interconnect/axi4_decoder.sv
+++ src/hdl_top/axi4_interconnect/axi4_decoder.sv
@@ -474,6 +474,7 @@ $display("select_master inisde iswrite == 1");
                     (map_slave_addr(m_awaddr[m]) == ($clog2(NO_OF_SLAVES)+1)'(targetSlave)) : 0;
                 $display("localWriteReq[%0d] = %0d | m_awvalid[%0d] = %0d ",m,localWriteReq[m] ,m,m_awvalid[m] );
                 $display(" m_awaddr[%0d] = %0d ",m,m_awaddr[m]);
+                $display("map_slave_addr(m_awaddr[%0d]) = %0d | targetSlave =%0d $clog2(NO_OF_SLAVES)+1)'(targetSlave) = %0d ",m,map_slave_addr(m_awaddr[m]),targetSlave,($clog2(NO_OF_SLAVES)+1)'(targetSlave)));
             end
             for (int m = 0; m < NO_OF_MASTERS; m++) begin
                 if (localWriteReq[m]) begin
