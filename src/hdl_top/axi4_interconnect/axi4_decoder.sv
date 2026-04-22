ee2ac97c42a93b115b494da32d28cfcfff4467b3
Sharath Kumar <143931064+SharathKumar246@users.noreply.github.com>
2026-04-22 17:29:24 +0530

    Update axi4_decoder.sv

--- src/hdl_top/axi4_interconnect/axi4_decoder.sv
+++ src/hdl_top/axi4_interconnect/axi4_decoder.sv
@@ -414,6 +414,8 @@ module axi4_decoder #(
                  $display("localWriteReq[%0d] = %0d | m_awvalid[%0d] = %0d ",m,localWriteReq[m] , m, m_awvalid[m] );
                 $display(" m_awaddr[%0d] = %0d ",m,m_awaddr[m]);
                 $display("map_slave_addr(m_awaddr[%0d]) = %0d | targetSlave =%0d $clog2(NO_OF_SLAVES)+1)'(targetSlave) = %0d ",m,map_slave_addr(m_awaddr[m]),targetSlave,($clog2(NO_OF_SLAVES)+1)'(targetSlave)); 
+                if(m_awvalid[m])
+                    $display(" m_awvalid[%b] = %b ",m,m_awvalid[m]);
             end
  
             for (int m = 0; m < NO_OF_MASTERS; m++) begin
