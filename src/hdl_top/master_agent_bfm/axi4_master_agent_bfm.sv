c6712ad7addeadf8b1eb876898d0a8861153dd3c
Sharath Kumar <143931064+SharathKumar246@users.noreply.github.com>
2026-04-23 16:44:26 +0530

    Update axi4_master_agent_bfm.sv

--- src/hdl_top/master_agent_bfm/axi4_master_agent_bfm.sv
+++ src/hdl_top/master_agent_bfm/axi4_master_agent_bfm.sv
@@ -114,8 +114,8 @@ axi4_master_driver_bfm axi4_master_drv_bfm_h (.aclk(intf.aclk),
   // Setting the virtual handle of BMFs into config_db
   //-------------------------------------------------------
   initial begin
-    uvm_config_db#(virtual axi4_master_driver_bfm)::set(null,"*", "axi4_master_driver_bfm", axi4_master_drv_bfm_h); 
-    uvm_config_db#(virtual axi4_master_monitor_bfm)::set(null,"*", "axi4_master_monitor_bfm", axi4_master_mon_bfm_h);
+    uvm_config_db#(virtual axi4_master_driver_bfm)::set( null, "*", $sformatf("axi4_master_driver_bfm_%0d", MASTER_ID), axi4_master_drv_bfm_h);
+    uvm_config_db#(virtual axi4_master_monitor_bfm)::set(null, "*", $sformatf("axi4_master_monitor_bfm_%0d", MASTER_ID), axi4_master_mon_bfm_h);
   end
 /*
   bind axi4_master_monitor_bfm master_assertions M_A (.aclk(aclk),
