c687f214f34c2a886433ce3e932d69c621a67d4b
Sharath Kumar <143931064+SharathKumar246@users.noreply.github.com>
2026-04-23 16:11:22 +0530

    Update axi4_globals_pkg.sv

--- src/globals/axi4_globals_pkg.sv
+++ src/globals/axi4_globals_pkg.sv
@@ -20,7 +20,7 @@ package axi4_globals_pkg;
 
   //Parameter: NO_OF_MASTERS
   //Used to set number of masters required
-  parameter int NO_OF_MASTERS = 2;
+  parameter int NO_OF_MASTERS = 3;
 
   //Parameter: NO_OF_SLAVES
   //Used to set number of slaves required
