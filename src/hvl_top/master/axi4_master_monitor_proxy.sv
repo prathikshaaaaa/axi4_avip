00c38c530ce7f43fe728d267a5304e6a2e630a9b
Sharath Kumar <143931064+SharathKumar246@users.noreply.github.com>
2026-04-23 17:56:18 +0530

    Update axi4_master_monitor_proxy.sv

--- src/hvl_top/master/axi4_master_monitor_proxy.sv
+++ src/hvl_top/master/axi4_master_monitor_proxy.sv
@@ -109,10 +109,13 @@ endfunction : connect_phase
 //--------------------------------------------------------------------------------------------
 function void axi4_master_monitor_proxy::end_of_elaboration_phase(uvm_phase phase);
   super.end_of_elaboration_phase(phase);
-  axi4_master_mon_bfm_h.axi4_master_mon_proxy_h = this;
+
    if(!uvm_config_db #(virtual axi4_master_monitor_bfm)::get(this,"",$sformatf("axi4_master_monitor_bfm_%0d", axi4_master_agent_cfg_h.master_id),axi4_master_mon_bfm_h)) begin
     `uvm_fatal("FATAL_MDP_CANNOT_GET_AXI4_MASTER_MONITOR_BFM","cannot get() axi4_master_mon_bfm_h");
   end 
+  
+  axi4_master_mon_bfm_h.axi4_master_mon_proxy_h = this;
+  
 endfunction : end_of_elaboration_phase
 
 
