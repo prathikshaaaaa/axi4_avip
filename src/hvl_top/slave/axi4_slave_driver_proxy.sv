9b6cbe4b78c0a789e17261585202240b91a28b06
Sharath Kumar <143931064+SharathKumar246@users.noreply.github.com>
2026-04-23 17:51:52 +0530

    Update axi4_slave_driver_proxy.sv

--- src/hvl_top/slave/axi4_slave_driver_proxy.sv
+++ src/hvl_top/slave/axi4_slave_driver_proxy.sv
@@ -142,9 +142,6 @@ endfunction : new
 //--------------------------------------------------------------------------------------------
 function void axi4_slave_driver_proxy::build_phase(uvm_phase phase);
   super.build_phase(phase);
-  if(!uvm_config_db#(virtual axi4_slave_driver_bfm)::get(this,"",$sformatf("axi4_slave_driver_bfm_%0d", axi4_slave_agent_cfg_h.slave_id),axi4_slave_drv_bfm_h))begin
-    `uvm_fatal("FATAL_MDP_CANNOT_GET_tx_DRIVER_BFM","cannot get() axi4_slave_drv_bfm_h");
-  end
 endfunction : build_phase
 
 //--------------------------------------------------------------------------------------------
@@ -159,6 +156,9 @@ function void axi4_slave_driver_proxy::end_of_elaboration_phase(uvm_phase phase)
     axi4_slave_mem_h = axi4_slave_memory::type_id::create("axi4_slave_mem_h");
   end
   axi4_slave_drv_bfm_h.axi4_slave_drv_proxy_h= this;
+    if(!uvm_config_db#(virtual axi4_slave_driver_bfm)::get(this,"",$sformatf("axi4_slave_driver_bfm_%0d", axi4_slave_agent_cfg_h.slave_id),axi4_slave_drv_bfm_h))begin
+    `uvm_fatal("FATAL_MDP_CANNOT_GET_tx_DRIVER_BFM","cannot get() axi4_slave_drv_bfm_h");
+  end
 endfunction  : end_of_elaboration_phase
 
 
