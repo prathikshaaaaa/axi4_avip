573acac4fa8bc55733448d22bc1a72260aca92d8
Sharath Kumar <143931064+SharathKumar246@users.noreply.github.com>
2026-04-23 16:22:34 +0530

    Update axi4_decoder.sv

--- src/hdl_top/axi4_interconnect/axi4_decoder.sv
+++ src/hdl_top/axi4_interconnect/axi4_decoder.sv
@@ -270,7 +270,7 @@ module axi4_decoder #(
                 master_aw_order[m].delete();
                 wr_w_slave[m] = -1;
                 m_awvalid[m] = '0;
-                m_awready[m] = '0;
+                m_awaddr[m] = '0;
             end
         end else begin
             for (int m = 0; m < NO_OF_MASTERS; m++) begin
