aa86246167f9d273933110807a3907b078f66a73
Swasthikkamath <118376716+Swasthikkamath@users.noreply.github.com>
2026-04-30 12:10:28 +0530

    preventing infinite run

--- src/hvl_top/test/virtual_sequences/axi4_virtual_stress_seq.sv
+++ src/hvl_top/test/virtual_sequences/axi4_virtual_stress_seq.sv
@@ -22,47 +22,51 @@ class axi4_virtual_stress_seq extends axi4_virtual_base_seq;
     // ---------------------------------------------------------
     // 2. BOOT UP ALL SLAVES (BACKGROUND THREADS)
     // ---------------------------------------------------------
-    for (int s = 0; s < NO_OF_SLAVES; s++) begin
-      automatic int slv_idx;
+    fork
+    begin 
+      for (int s = 0; s < NO_OF_SLAVES; s++) begin
+        automatic int slv_idx;
       
-      s_wb_seq[s]     = axi4_slave_writeback_seq::type_id::create($sformatf("s_wb_seq[%0d]", s));
-      s_refill_seq[s] = axi4_slave_refill_seq::type_id::create($sformatf("s_refill_seq[%0d]", s));
+        s_wb_seq[s]     = axi4_slave_writeback_seq::type_id::create($sformatf("s_wb_seq[%0d]", s));
+        s_refill_seq[s] = axi4_slave_refill_seq::type_id::create($sformatf("s_refill_seq[%0d]", s));
       
-      slv_idx = s;
+        slv_idx = s;
       
-      fork
-        forever s_wb_seq[slv_idx].start(p_sequencer.axi4_slave_write_seqr_h[slv_idx]);
-        forever s_refill_seq[slv_idx].start(p_sequencer.axi4_slave_read_seqr_h[slv_idx]);
-      join_none
-    end
-
+        fork
+          forever s_wb_seq[slv_idx].start(p_sequencer.axi4_slave_write_seqr_h[slv_idx]);
+          forever s_refill_seq[slv_idx].start(p_sequencer.axi4_slave_read_seqr_h[slv_idx]);
+        join_none
+      end
+    end 
     // ---------------------------------------------------------
     // 3. FIRE ALL MASTERS AT THE EXACT SAME TIME
     // ---------------------------------------------------------
-    for (int m = 0; m < NO_OF_MASTERS; m++) begin
-
-      automatic int mst_idx;
+    begin 
+      for (int m = 0; m < NO_OF_MASTERS; m++) begin
+  
+        automatic int mst_idx;
       
-      m_seq[m] = axi4_master_write_miss_seq::type_id::create($sformatf("m_seq[%0d]", m));
+        m_seq[m] = axi4_master_write_miss_seq::type_id::create($sformatf("m_seq[%0d]", m));
       
       // Randomize each master to target different random addresses
-      if(!m_seq[m].randomize()) begin
-         `uvm_fatal("STRESS_SEQ", "Master randomization failed")
-      end
+        if(!m_seq[m].randomize()) begin
+           `uvm_fatal("STRESS_SEQ", "Master randomization failed")
+        end
 
-       mst_idx = m;
+        mst_idx = m;
       
-      fork
-        // This fires the masters in parallel
-        m_seq[mst_idx].start(p_sequencer.axi4_master_write_seqr_h[mst_idx]);
-      join_none
-    end
+        fork
+          // This fires the masters in parallel
+          m_seq[mst_idx].start(p_sequencer.axi4_master_write_seqr_h[mst_idx]);
+        join_none
+      end
 
     // 4. WAIT FOR ALL MASTERS TO FINISH
     // This blocks until all CPU sequences have completed.
     // We do NOT wait for the slaves, because they are running forever!
-    wait fork;
-
+      wait fork;
+    end 
+    join
     `uvm_info(get_type_name(), "MULTI-MASTER STRESS TEST COMPLETE", UVM_LOW)
   endtask
 
