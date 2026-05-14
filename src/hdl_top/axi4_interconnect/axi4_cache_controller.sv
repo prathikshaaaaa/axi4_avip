c579d4b89287beed91532f17fe6141c59560843e
tejas-poojary <tejaspoojary2003@gmail.com>
2026-05-14 17:56:46 +0530

    Refactor display message in WRITE RESPONSE GENERATION

--- src/hdl_top/axi4_interconnect/axi4_cache_controller.sv
+++ src/hdl_top/axi4_interconnect/axi4_cache_controller.sv
@@ -1226,8 +1226,7 @@ end
         wr_resp[m]       = 2'b00;
         wr_resp_id[m]    = wr_hit_id[m];
         $display("inside !has_mshr");
-        $display("%0t inisde WRITE RESPONSE GENERATION  :if (!has_mshr && wr_cache_hit[m] && wr_data_last_g[m] && (m[$clog2(NO_OF_SLAVES)-1:0] == w_owner))  master = %d ",$time,m);
-      end
+        $display("%0t inside WRITE RESPONSE GENERATION: hit path !has_mshr && wr_cache_hit[m] && wr_data_last_g[m] && w_locked[m] master=%0d", $time, m);
     end
   end
 
