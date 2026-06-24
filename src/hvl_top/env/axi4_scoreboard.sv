3a19df0ca9aaf063a10e4a86b1acc7794408ad79
tejas-poojary <tejaspoojary2003@gmail.com>
2026-06-24 11:57:17 +0530

    Add delay before handling write request

--- src/hvl_top/env/axi4_scoreboard.sv
+++ src/hvl_top/env/axi4_scoreboard.sv
@@ -1814,6 +1814,7 @@ foreach(axi4_master_write_address_analysis_fifo[i]) begin
       //    MISS → MSHR allocated (is_write=1)
       //    ref_model_write must NOT call this again.
       //=========================================================
+      #0;
       l3_handle_write_request(m_idx, m_write_addr_tx, s_idx);
 
       //=========================================================
