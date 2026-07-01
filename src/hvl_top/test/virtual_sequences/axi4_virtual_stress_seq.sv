8675cf16beacc1915187173fb2f7f53fc6c234a7
tejas-poojary <tejaspoojary2003@gmail.com>
2026-07-01 11:50:10 +0530

    Update axi4_virtual_stress_seq.sv

--- src/hvl_top/test/virtual_sequences/axi4_virtual_stress_seq.sv
+++ src/hvl_top/test/virtual_sequences/axi4_virtual_stress_seq.sv
@@ -26,11 +26,9 @@ class axi4_virtual_stress_seq extends axi4_virtual_base_seq;
     begin 
       for (int s = 0; s < NO_OF_SLAVES; s++) begin
         automatic int slv_idx;
-      
+        slv_idx = s;
         s_wb_seq[s]     = axi4_slave_writeback_seq::type_id::create($sformatf("s_wb_seq[%0d]", s));
         s_refill_seq[s] = axi4_slave_refill_seq::type_id::create($sformatf("s_refill_seq[%0d]", s));
-      
-        slv_idx = s;
       
         fork
           forever s_wb_seq[slv_idx].start(p_sequencer.axi4_slave_write_seqr_h[slv_idx]);
@@ -43,8 +41,8 @@ class axi4_virtual_stress_seq extends axi4_virtual_base_seq;
     // ---------------------------------------------------------
     begin 
       for (int m = 0; m < NO_OF_MASTERS; m++) begin
-  
         automatic int mst_idx;
+        mst_idx = m;
       
         m_seq[m] = axi4_master_write_miss_seq::type_id::create($sformatf("m_seq[%0d]", m));
         m_seq[m].txn_num = m;
@@ -52,8 +50,6 @@ class axi4_virtual_stress_seq extends axi4_virtual_base_seq;
         if(!m_seq[m].randomize()) begin
            `uvm_fatal("STRESS_SEQ", "Master randomization failed")
         end
-
-        mst_idx = m;
       
         fork
           // This fires the masters in parallel
