3c550a27f0f8545c5d5144a14eaec7bf67b3b6c0
prathikshaaaaa <prathikshag007@gmail.com>
2026-08-06 07:21:06 +0000

    rlast capture

--- src/hvl_top/master/axi4_master_seq_item_converter.sv
+++ src/hvl_top/master/axi4_master_seq_item_converter.sv
@@ -309,7 +309,7 @@ function void axi4_master_seq_item_converter::to_read_class( input axi4_read_tra
   
   $cast(output_conv_h.rid,input_conv_h.rid);
   `uvm_info("axi4_master_seq_item_conv_class",$sformatf("After converting rid =  %b",output_conv_h.rid),UVM_FULL);
-
+output_conv_h.rlast = input_conv_h.rlast;
   foreach(input_conv_h.rdata[i]) begin
     if(input_conv_h.rdata[i] != 'h0)begin
       output_conv_h.rdata.push_front(input_conv_h.rdata[i]);
